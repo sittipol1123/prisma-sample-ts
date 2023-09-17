2c6cb596adbaa8909bc7bfa2592300342ed521f0
stp.do <sittipol1123@gmail.com>
2023-09-17 13:32:22 +0700

    ..

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 const app: Express = express();
 const port: number = 3333;
 
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ message: "hello" });
+app.get("/", async (req: Request, res: Response) => {
+    const data = await prisma.user.findMany({
+        include: {
+            posts: true
+        }
+    })
+  return res.json({ message: "hello", data });
 });
 
 app.listen(port, () => {
-    console.log('app is started');
-})
+  console.log("app is started");
+});
--- index.ts
+++ index.ts
@@ -3,28 +3,33 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.create({
-        data: {
-            name: 'sittipol',
-            email: 'sittipol@gmail.com',
-            posts: {
-                create: {
-                    title: 'stp',
-                    content: 'th'
-                }
-            }
-        }
-    })
-    const user = await prisma.user.findMany();
-    console.log(user);
+  // await prisma.user.create({
+  //     data: {
+  //         name: 'sittipol',
+  //         email: 'sittipol@gmail.com',
+  //         posts: {
+  //             create: {
+  //                 title: 'stp',
+  //                 content: 'th'
+  //             }
+  //         }
+  //     }
+  // })
+
+  const data = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(data, { depth: null });
 }
 
 main()
-.then(async () => {
+  .then(async () => {
     await prisma.$connect();
-})
-.catch(async (e) => {
-    console.log(e)
+  })
+  .catch(async (e) => {
+    console.log(e);
     await prisma.$disconnect();
     process.exit(1);
-})
+  });
